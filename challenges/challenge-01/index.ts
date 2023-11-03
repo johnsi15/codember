c4e44457a62ad37484eac41ab3dfd9d0772c26fb
John Serrano <andrey_201015@hotmail.com>
2023-11-03 11:41:13 -0500

    chore(Root): Setup project and first challenge

---
+++ challenges/challenge-01/index.ts
@@ -0,0 +1,35 @@
+import { readFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+
+export type WordCount = {
+  [word: string]: number
+}
+
+async function messages() {
+  try {
+    const filePath = resolve('./message_01.txt')
+    const wordList = await readFile(filePath, { encoding: 'utf8' })
+
+    let wordsCount: WordCount = {}
+    let resultMessage = ''
+
+    wordList.split(' ').forEach(word => {
+      const wordSanitized = word.toLowerCase()
+      wordsCount[wordSanitized] !== undefined ? (wordsCount[wordSanitized] += 1) : (wordsCount[wordSanitized] = 1)
+    })
+
+    const words = Object.keys(wordsCount)
+
+    for (const word of words) {
+      resultMessage += `${word}${wordsCount[word]}`
+    }
+
+    console.log(resultMessage)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+;(async () => {
+  await messages()
+})()

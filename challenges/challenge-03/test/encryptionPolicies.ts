01e09cfd6078d62023949bb1568cf26685f52496
John Serrano <andrey_201015@hotmail.com>
2023-11-21 16:41:57 -0500

    Changes solution challenge 03 and add test

--- challenges/challenge-03/test/encryptionPolicies.ts
+++ challenges/challenge-03/test/encryptionPolicies.ts
@@ -1,19 +1,9 @@
-import { readFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
-
-async function encryptionPolicies() {
-  let passwords = ''
-
-  try {
-    const filePath = resolve('./encryption_policies.txt')
-    passwords = await readFile(filePath, { encoding: 'utf8' })
-  } catch (error) {
-    console.log('This is error read file -> ', error)
-  }
-
+export function encryptionPolicies(passwords: string) {
   const listPasswords = passwords.split('\n')
 
-  const INDEXPASSWORD = 42 // or 13
+  const INDEXPASSWORD = 1
+  let invalidPassword = ''
+  let validCountPassword = 0
   let invalidCountPassword = 0
 
   listPasswords.forEach(text => {
@@ -27,12 +17,12 @@ async function encryptionPolicies() {
     if (!(occurrences >= Number(minWord) && occurrences <= Number(maxWord))) {
       invalidCountPassword++
       if (invalidCountPassword === INDEXPASSWORD) {
-        console.log('invalid password is -> ' + passwordValue)
+        invalidPassword = passwordValue
       }
+    } else {
+      validCountPassword++
     }
   })
-}
 
-;(async () => {
-  await encryptionPolicies() // result -> bgamidqewtbus
-})()
+  return { invalidPassword, validCountPassword }
+}

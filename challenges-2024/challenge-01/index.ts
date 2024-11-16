26233b24361218e8589dc1a568709022499a9f8c
John Serrano <andrey_201015@hotmail.com>
2024-11-15 22:03:41 -0500

    Changes challenge 01

--- challenges-2024/challenge-01/index.ts
+++ challenges-2024/challenge-01/index.ts
@@ -1,4 +1,5 @@
 function accessTerminal({ code }: { code: string }) {
+  // v2
   const [nums, moves] = code.split(' ')
   const result = nums.split('').map(Number)
 
@@ -23,3 +24,26 @@ accessTerminal({ code: '528934712834 URDURUDRUDLLLLUUDDUDUDUDLLRRRR' }) // 62893
 // accessTerminal({ code: '000 URURD' }) // 119
 // accessTerminal({ code: '1111 UUURUUU' }) // 4411
 // accessTerminal({ code: '9999 LULULULD' }) // 8000
+
+// V1
+function accessTerminalWithoutCyclic({ code }: { code: string }) {
+  const [nums, moves] = code.split(' ')
+  const result = nums.split('').map(Number)
+  let position = 0
+
+  for (let i = 0; i < moves.length; i++) {
+    const move = moves[i]
+
+    if (move === 'U') {
+      result[position] = (result[position] + 1) % 10
+    } else if (move === 'D') {
+      result[position] = (result[position] - 1 + 10) % 10
+    } else if (move === 'R') {
+      position++
+    } else if (move === 'L') {
+      position--
+    }
+  }
+
+  console.log({ result: result.join('') })
+}

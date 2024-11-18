b1f95a684827ed28d78390a4696ff34f95f78e9f
John Serrano <andrey_201015@hotmail.com>
2024-11-18 18:52:22 -0500

    Add solution challenge 02

--- challenges-2024/challenge-02/index.ts
+++ challenges-2024/challenge-02/index.ts
@@ -7,11 +7,42 @@ function access() {
       return
     }
 
-    // console.log(data.split('\n'))
+    let passwordFalsy = 0
+    let passwordTruthy = 0
+
+    function isOnlyLettersAndNumbers(text: string) {
+      return /^[a-z0-9]+$/.test(text)
+    }
+
+    function isUpNumber(text: string) {
+      return /^(?:0*1*2*3*4*5*6*7*8*9*)$/.test(text)
+    }
+
+    function isUpLetter(text: string) {
+      return /^(?:a*b*c*d*e*f*g*h*i*j*k*l*m*n*o*p*q*r*s*t*u*v*w*x*y*z*)$/.test(text)
+    }
+
+    function isValidPassword(password: string) {
+      const parts = password.match(/^([0-9]*)([a-z]*)$/)
+      if (!parts) return false
+
+      const [_, numbers, letters] = parts
+      return isUpNumber(numbers) && isUpLetter(letters)
+    }
 
     for (const code of data.split('\n')) {
-      console.log({ code })
+      if (isOnlyLettersAndNumbers(code)) {
+        if (isValidPassword(code)) {
+          passwordTruthy++
+        } else {
+          passwordFalsy++
+        }
+      } else {
+        passwordFalsy++
+      }
     }
+
+    console.log(`${passwordTruthy}true${passwordFalsy}false`)
   })
 }
 

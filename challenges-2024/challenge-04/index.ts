6121bc4b834530ce31ea927c46a0c35911fade54
John Serrano <andrey_201015@hotmail.com>
2024-11-27 18:40:18 -0500

    Solution v2 challenge 04

--- challenges-2024/challenge-04/index.ts
+++ challenges-2024/challenge-04/index.ts
@@ -1,40 +1,53 @@
 import fs from 'node:fs'
 
 function getNetworkSaves() {
-  fs.readFile('./test.txt', 'utf8', (err, data) => {
+  fs.readFile('./network.txt', 'utf8', (err, data) => {
     const networks = JSON.parse(data)
     console.log({ networks })
-    const nodos = new Map()
+    // const nodos = new Map()
+    interface Nodos {
+      [key: string]: number[]
+    }
+
+    const nodos: Nodos = {}
 
     let result = ''
 
     for (let i = 0; i < networks.length; i++) {
-      const network = networks[i]
-      const network2 = networks[i + 1]
-      console.log({ network, network2 })
-      if (network2 !== undefined) {
-        const second = network[1]
-        const first = network2[0] ?? ''
-        console.log({ second, first })
-      }
-      // for (let j = 0; j < network.length; j++) {
-      //   const nodo = network[j]
-      //   console.log({ primary: network[0], second })
-      //   console.log()
-      //   if (second !== network[0]) {
-      //     console.log('paso')
-      //     console.log({ network: network[i] })
-      //     result = network.join(',') + result
-      //   }
-      //   // if (nodos.has(nodo)) {
-      //   //   nodos.set(nodo, [nodos.get(nodo), i])
-      //   // } else {
-      //   //   nodos.set(nodo, i)
-      //   // }
-      // }
-
-      // console.log({ nodos })
+      const nodo = networks[i]
+      console.log({ nodo })
+      nodo.forEach((n: number) => {
+        if (n in nodos) {
+          nodos[n] = [...nodos[n], i]
+        } else {
+          nodos[n] = [i]
+        }
+      })
     }
+
+    console.log({ nodos })
+    let indicesToRemove: number[] = []
+
+    for (const key in nodos) {
+      if (nodos[key].length > 1) {
+        nodos[key].forEach(i => {
+          indicesToRemove.push(i)
+        })
+      }
+    }
+
+    indicesToRemove = [...new Set(indicesToRemove)]
+    indicesToRemove.sort((a, b) => b - a)
+
+    indicesToRemove.forEach(index => {
+      networks.splice(index, 1)
+    })
+
+    networks.forEach((n: number[]) => {
+      // console.log(n.join(','))
+      result += n.join(',') + ','
+    })
+
     console.log({ result })
   })
 }
